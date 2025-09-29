ff1e5fe7610a289bee9f297a144c8fb9552af396
mauricin-vm <mauricio-vm@hotmail.com>
2025-09-29 15:52:43 -0400

    [fix]: auth option

--- lib/auth/config.ts
+++ lib/auth/config.ts
@@ -97,7 +97,7 @@ export const authOptions: NextAuthOptions = {
         httpOnly: true,
         sameSite: 'lax',
         path: '/',
-        secure: process.env.NODE_ENV === 'production'
+        secure: false
       }
     }
   },
